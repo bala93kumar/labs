cff141341c2b1973b6f85dd4738d2ebd0bcc3e0f
Balakumar Seetharaman <bala93kumar@gmail.com>
2025-12-23 22:41:16 +0400

    docker command with env

--- docker/node-app/server.js
+++ docker/node-app/server.js
@@ -8,4 +8,7 @@ app.get('/', (req, res) => {
   `);
 })
 
-app.listen(3000);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
